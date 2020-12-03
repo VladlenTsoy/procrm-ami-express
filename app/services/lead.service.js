8411b1cf0b1934d9c047fdf10adc725b1eabdf1a
vladlen <vladlengg@gmail.com>
2020-12-03 11:10:25 +0500

    fix no lead lead.service.js

--- app/services/lead.service.js
+++ app/services/lead.service.js
@@ -3,12 +3,13 @@ const {Lead} = require('models/Lead')
 const FindContactAndDial = async (tel) => {
     if (String(tel).length >= 9) {
         const lead = await Lead.query().findOne('phonenumber', 'LIKE', `%${tel}%`)
-        return {
-            id: lead.id,
-            name: lead.name,
-            phonenumber: lead.phonenumber,
-            tel: tel,
-        }
+        if (lead)
+            return {
+                id: lead.id,
+                name: lead.name,
+                phonenumber: lead.phonenumber,
+                tel: tel,
+            }
     }
     return {
         id: null,
