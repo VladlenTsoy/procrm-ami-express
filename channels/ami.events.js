f7e0419a6874a7913ac7816f9fe8af7a8b2acc66
vladlen <vladlengg@gmail.com>
2020-12-03 14:32:35 +0500

    update ami.events.js

--- channels/ami.events.js
+++ channels/ami.events.js
@@ -3,6 +3,9 @@ const LeadService = require('services/lead.service')
 
 const Connect = (socket) => {
     const sip = socket.handshake.query.sip
+    let currentChannel = null
+    let currentExten = null
+
     if (ami.connected)
         socket.emit(`ami_connect`, {status: 'success', message: 'success'})
 
@@ -11,39 +14,71 @@ const Connect = (socket) => {
         socket.emit(`ami_connect`, {status: 'success', message: 'success'})
     });
 
-    // Принятый звонок
+    // Принятый звонок локальный
     ami.on('event', async function (event) {
         // Принятый звонок
-        if (event.event === 'Newstate' && event.connectedlinenum === sip && event.calleridnum !== sip) {
-            const lead = await LeadService.FindContactAndDial(event.calleridnum)
-            socket.emit(`ami_newstate`, {
-                lead,
-                info: {
-                    channelstate: event.channelstate,
-                    calleridnum: event.calleridnum,
-                    uniqueid: event.uniqueid,
-                    channel: event.channel,
-                },
-            })
-        }
+        if (
+            event.event === 'Newstate' &&
+            event.context === 'sip-locals-callPermissions'
+        ) {
+            if (
+                event.connectedlinenum === sip &&
+                event.calleridnum !== sip
+            ) {
+                const lead = await LeadService.FindContactAndDial(event.calleridnum)
+                socket.emit(`ami_newstate`, {
+                    lead,
+                    info: {
+                        channelstate: event.channelstate,
+                        channelstatedesc: event.channelstatedesc,
+                        calleridnum: event.calleridnum,
+                        uniqueid: event.uniqueid,
+                        channel: event.channel,
+                    },
+                })
+            } else if (
+                event.connectedlinenum !== sip &&
+                event.calleridnum === sip
+            ) {
+                currentChannel = event.channel
+                currentExten = event.connectedlinenum !== '<unknown>' ?
+                    event.connectedlinenum :
+                    event.exten
 
-        // Принятый звонок
-        if (event.event === 'Newstate' && event.connectedlinenum !== sip && event.calleridnum === sip) {
-            const lead = await LeadService.FindContactAndDial(event.connectedlinenum)
-            socket.emit(`ami_newstate`, {
-                lead,
-                info: {
-                    channelstate: event.channelstate,
-                    calleridnum: event.connectedlinenum,
-                    uniqueid: event.uniqueid,
-                    channel: event.channel,
-                },
-            })
+                const lead = await LeadService.FindContactAndDial(currentExten)
+
+                socket.emit(`ami_newstate`, {
+                    lead,
+                    info: {
+                        channelstate: event.channelstate,
+                        channelstatedesc: event.channelstatedesc,
+                        calleridnum: event.exten,
+                        uniqueid: event.uniqueid,
+                        channel: event.channel,
+                    },
+                })
+            } else if (
+                event.channel === currentChannel &&
+                event.calleridnum !== sip
+            ) {
+                const lead = await LeadService.FindContactAndDial(currentExten)
+                socket.emit('ami_newstate', {
+                    lead,
+                    info: {
+                        channelstate: event.channelstate,
+                        channelstatedesc: event.channelstatedesc,
+                        calleridnum: currentExten,
+                        uniqueid: event.uniqueid,
+                        channel: event.channel,
+                    },
+                })
+            }
         }
 
         // Сбросить звонок
-        if (event.event === 'Hangup' && event.channel.includes('SIP/' + sip))
+        if (event.event === 'Hangup' && event.channel.includes('SIP/' + sip)) {
             socket.emit(`ami_event_hangup`, {})
+        }
     })
 }
 
